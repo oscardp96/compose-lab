9f45c5e7a51835beb7ea0ff5e853d7315bc5f636
oscardariasvf <oscar.darias@vodafone.com>
2021-08-31 09:41:50 +0200

    Working with custom layouts now

--- ComposeLayouts/app/src/main/java/com/example/composelayouts/MainActivity.kt
+++ ComposeLayouts/app/src/main/java/com/example/composelayouts/MainActivity.kt
@@ -3,16 +3,24 @@ package com.example.composelayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.composelayouts.ui.theme.ComposeLayoutsTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +46,7 @@ fun LayoutsCodelab() {
                 CodelabBottomNavigationBar()
         }
     ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding))
+        LazyList(Modifier.padding(innerPadding))
     }
 }
 
@@ -59,18 +67,82 @@ fun CodelabTopAppBar(modifier: Modifier = Modifier) {
 
 @Composable
 fun CodelabBottomNavigationBar(modifier: Modifier = Modifier) {
-    BottomNavigation(modifier = modifier.fillMaxSize()) {
-        Button(modifier = Modifier.align(CenterVertically), onClick = { /* empty */ }) {
+    BottomNavigation(modifier = modifier) {
+        Button(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .fillMaxWidth()
+                .padding(10.dp),
+            onClick = { /* empty */ }
+        ) {
             Text(text = "A button on the bottom")
         }
     }
 }
 
 @Composable
-fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(8.dp)) {
-        Text(text = "Hi there!")
-        Text(text = "Going through the Layouts Codelab")
+fun LazyList(modifier: Modifier = Modifier) {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    
+    Column(modifier = modifier) {
+        ScrollingButtons(coroutineScope = coroutineScope, scrollState = scrollState, listSize = listSize)
+        LazyColumn(state = scrollState) {
+            items(100) {
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ScrollingButtons(
+    coroutineScope: CoroutineScope,
+    scrollState: LazyListState,
+    listSize: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier.fillMaxWidth()) {
+        Button(
+            modifier = Modifier.fillMaxWidth(fraction = 0.5f),
+            onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }
+        ) {
+            Text("Scroll to the top")
+        }
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }
+        ) {
+            Text("Scroll to the end")
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
